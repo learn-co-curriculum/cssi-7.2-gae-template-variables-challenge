747b986ba5416ce23efad7423ddee5761b9812ab
nanselmo <nanselmo@nanselmo-macbookpro.roam.corp.google.com>
2015-06-29 10:24:01 -0500

    initial commit

---
+++ helloworld.py
@@ -0,0 +1,19 @@
+import jinja2
+import os
+import webapp2
+
+jinja_environment = jinja2.Environment(loader=
+    jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+class HelloHandler(webapp2.RequestHandler):
+  def get(self):
+    template_vars = {"name": self.request.get('name')}
+    template = jinja_environment.get_template('templates/hello.html')
+    self.response.out.write(template.render(template_vars))
+
+routes = [  #a list of tuples mapping paths to handlers
+
+    ('/.*', HelloHandler),
+]
+
+app = webapp2.WSGIApplication(routes, debug=True)
